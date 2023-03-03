0c9804828d7318e707decd0596484107771141fd
Ravi Teja <ravi_teja_1797@yahoo.com>
2023-03-03 06:43:35 +0000

    Successfully defined all the APIs

--- app.js
+++ app.js
@@ -63,6 +63,85 @@ expressAppInstance.get("/movies/:movieId/", async (request, response) => {
   let { movieId } = request.params;
   movieId = parseInt(movieId);
   const getMovieQuery = `SELECT * FROM movie WHERE movie_id = ${movieId}`;
-  let getMovieResponse = await databaseConnectionObject.get(getMovieQuery);
-  console.log(getMovieResponse);
+  let movieObject = await databaseConnectionObject.get(getMovieQuery);
+  
+  let tempObject ={
+        movieId : movieObject["movie_id"],
+        directorId : movieObject['director_id'],
+        movieName : movieObject["movie_name"],
+        leadActor : movieObject["lead_actor"]
+   }
+    response.send(tempObject)
 });
+
+//API -4 updateMovie
+
+expressAppInstance.put("/movies/:movieId/", async (request, response) => {
+  let { movieId } = request.params;
+  movieId = parseInt(movieId);
+
+  const movieDetails = request.body;
+  const { directorId, movieName, leadActor } = movieDetails;
+
+  const updateMovieQuery = `UPDATE movie SET director_id = ${directorId}, movie_name = "${movieName}", lead_actor = "${leadActor}" WHERE movie_id = ${movieId}`;
+  try {
+    await databaseConnectionObject.run(updateMovieQuery);
+    response.send("Movie Details Updated");
+  } catch (e) {
+    console.log(`SQL Error ${e.message}`);
+  }
+});
+
+//API-5 deleteMovie
+
+expressAppInstance.delete("/movies/:movieId/", async (request, response) => {
+  let { movieId } = request.params;
+  movieId = parseInt(movieId);
+
+  const deleteMovieQuery = `DELETE FROM movie WHERE movie_id = ${movieId}`;
+  try {
+    await databaseConnectionObject.run(deleteMovieQuery);
+    response.send("Movie Removed");
+  } catch (e) {
+    console.log(`Database Error ${e.message}`);
+  }
+});
+
+//API-6 getDirectorsList
+
+expressAppInstance.get("/directors/", async (request, response) => {
+  const getDirectorsQuery = `SELECT * FROM director`;
+  let directorsObjectsArray = await databaseConnectionObject.all(
+    getDirectorsQuery
+  );
+  directorsObjectsArray = directorsObjectsArray.map((eachObject) => {
+    let tempObject = {
+      directorId: eachObject["director_id"],
+      directorName: eachObject["director_name"],
+    };
+    return tempObject;
+  });
+  response.send(directorsObjectsArray);
+});
+
+//API-7 movieDirectorList
+
+expressAppInstance.get("/directors/:directorId/movies/", async(request, response)=>{
+    let {directorId} = request.params;
+    directorId = parseInt(directorId);
+    
+    const movieDirectorListQuery = `SELECT * FROM 
+    movie NATURAL JOIN director as movieDirector
+    WHERE movieDirector.director_id = ${directorId}`
+
+    let arrayOfMovies = await databaseConnectionObject.all(movieDirectorListQuery)
+    arrayOfMovies = arrayOfMovies.map((eachMovieObject)=>{
+        let tempObject = {
+            movieName: eachMovieObject['movie_name'],
+        }
+        return tempObject
+    })
+    response.send(arrayOfMovies)
+});
+
+module.exports = expressAppInstance;
